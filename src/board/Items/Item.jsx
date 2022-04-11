f23999cd909748d6cc28f8a8682db0fcaf34cded
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2022-04-11 22:06:29 +0200

    Change corner order

--- src/board/Items/Item.jsx
+++ src/board/Items/Item.jsx
@@ -283,8 +283,8 @@ const Item = ({
         </ItemErrorBoundary>
         <div className="corner top-left" />
         <div className="corner top-right" />
-        <div className="corner bottom-left" />
         <div className="corner bottom-right" />
+        <div className="corner bottom-left" />
         {isSelected && showResizeHandle && (
           <>
             {resizeDirections.b && (
