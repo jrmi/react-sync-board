4846b8a773a6f68c75a819c4e7615a0b625001a2
Jeremie Pardou <571533+jrmi@users.noreply.github.com>
2025-03-09 12:24:03 +0100

    Fix dim safe for good

--- src/lib/board/useDim.js
+++ src/lib/board/useDim.js
@@ -207,12 +207,11 @@ const useDim = () => {
 
       if (debug) console.log("New fixed values: ", newX, newY, newScale, newRotate);
 
-
       updateBoardState({
-        translateX: newX,
-        translateY: newY,
-        scale: newScale,
-        rotate: newRotate,
+        translateX: isNaN(newX)? 0: newX,
+        translateY: isNaN(newY)? 0: newY,
+        scale: isNaN(newScale)? clampScale(1): newScale,
+        rotate: isNaN(newRotate) ? 0 : newRotate,
       });
     },
     [clampScale, getBoardState, getConfiguration, updateBoardState]
@@ -287,15 +286,18 @@ const useDim = () => {
       const { rotate } = getBoardState();
       const { boardWrapperRect, boardSize } = getConfiguration();
 
-      const scaleX = boardWrapperRect.width / (radius * 2);
-      const scaleY = boardWrapperRect.height / (radius * 2);
+      const [safeX, safeY, safeRadius] = [x || 0, y||0, radius || 2000]
+
+      const scaleX = boardWrapperRect.width / (safeRadius * 2);
+      const scaleY = boardWrapperRect.height / (safeRadius * 2);
 
       // The scale that fits in all dimensions with a border around
       const scale = clampScale(Math.min(scaleX, scaleY) * SCALE_TOLERANCE);
 
+
       // We apply the board transformations
       const [translateX, translateY] = transformTo(
-        [-boardSize / 2 - x, -boardSize / 2 - y],
+        [-boardSize / 2 - safeX, -boardSize / 2 - safeY],
         {
           translateX: boardWrapperRect.width / 2,
           translateY: boardWrapperRect.height / 2,
